be1077939855d1205f37f00eb61dd907114b1815
Ina Pishmirova <inna.pishmirova@gmail.com>
2020-04-12 17:00:49 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -5,15 +5,63 @@ class Student {
 
 	List<Integer> labPoints = new List<Integer>();
 
-	//TODO constructor
+	Student(String index, String firstName, String lastName, List<Intsger> labPoints){
+	this.index=index;
+	this.firstName=first.Name;
+	this.lastName=lastName;
+	this.labPoints = labPoints;
 
-	//TODO seters & getters
+	}
+	void setList(List<Integer> l){
+		labpoints = l;
+	}
+	void setIndex(String i) {
+  		index=i;
+ 		 }
+	void setFirstName(String fn) {
+  		firstName=fn;
+ 		 }
+	void setLastName(String ln) {
+  		lastName=ln;
+ 		 }
+ 	List<Integer> get(){
+		return labPoints;
+		}
+	String getIndex(){
+		return index;
+		}
+	String getFirstName(){
+		return firstName;
+		}
+	String getLastName(){
+		return lastName;
+		}
+
 
 	public double getAverage() {
-		//TODO
+		ListNode<Integer> tmp  labPoints.first;
+		int zbir = 0;
+		While(tmp!=null)
+		{
+			Zbir += tmp;
+			Tmp = tmp.succ;
+		}
+		Return zbir/labPoints.lenght;
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		ListNode<Integer> tmp  labPoints.first;
+		int brojac = 0;
+		While(tmp!=null)
+		{
+			Brojac++;
+			Tmp = tmp.succ;
+		}
+		If(brojac >=8){
+			Return true;
+		}
+		else{
+		return false;
+		}
 	}
 }
